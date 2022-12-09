4e74e8f9905f239731a2d08b5fbd32d001a8f06d
SON <son@SONs-Mac-mini.local>
2022-12-09 14:40:04 +0700

    init

---
+++ lib/Extends/ColorExtends.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: file_names
+
+import 'dart:ui';
+
+class ColorExtends extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  ColorExtends(final String hexColor) : super(_getColorFromHex(hexColor));
+}
---
+++ lib/base_extends.dart
@@ -0,0 +1,9 @@
+library base_extends;
+
+export './Extends/export_extends.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
